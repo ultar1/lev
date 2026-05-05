a3fe39a0f310f42f3c0ebfb405d3b286f825c24e
ultar1 <138212013+ultar1@users.noreply.github.com>
2026-05-05 19:34:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -338,7 +338,7 @@ pm2.on('close', (code) => {
       scheduleRestart();
     }
     
-    if (out.includes('External Plugins Installed')) {
+    if (out.includes('External plugins installed! 🎒')) {
       const now = new Date().toLocaleString('en-GB',{ timeZone:'Africa/Lagos'});
       const message = `[${APP_NAME}] connected.\n🔐 ${SESSION_ID}\n🕒 ${now}`;
       await sendTelegramAlert(message, TELEGRAM_USER_ID);
